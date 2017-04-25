f37f085ec08f2009a2f694492ff85a5d34b313de
todorokit <fstest1234ifua@gmail.com>
2017-04-25 23:07:01 +0900

    refactoring

--- train.py
+++ train.py
@@ -31,63 +31,60 @@ flags.DEFINE_string('train', 'train.txt', 'File name of train data')
 flags.DEFINE_string('test', 'test.txt', 'File name of train data')
 flags.DEFINE_string('train_dir', 'c:\\tmp\\image_cnn', 'Directory to put the training data.')
 flags.DEFINE_integer('max_steps', 30, 'Number of steps to run trainer.')
-flags.DEFINE_integer('batch_size', 5, 'Batch size'
+flags.DEFINE_integer('batch_size', 20, 'Batch size'
                      'Must divide evenly into the dataset sizes.')
 flags.DEFINE_float('learning_rate', 1e-4, 'Initial learning rate.')
 
 def inference(images_placeholder, keep_prob):
-    """ 予測モデルを作成する関数
-
-    引数: 
-      images_placeholder: 画像のplaceholder
-      keep_prob: dropout率のplace_holder
-
-    返り値:
-      y_conv: 各クラスの確率(のようなもの)
-    """
+    """ 予測モデル keep_probはトレーニング時以外は1.0にする    """
+    
     # 重みを標準偏差0.1の正規分布で初期化
     def weight_variable(shape):
       initial = tf.truncated_normal(shape, stddev=0.1)
       return tf.Variable(initial)
 
-    # バイアスを標準偏差0.1の正規分布で初期化
+    # バイアスを0.1で初期化
     def bias_variable(shape):
       initial = tf.constant(0.1, shape=shape)
       return tf.Variable(initial)
 
-    # 畳み込み層の作成
+    # 畳み込み層
     def conv2d(x, W):
       return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
 
-    # プーリング層の作成
+    # プーリング層
     def max_pool_2x2(x):
       return tf.nn.max_pool(x, ksize=[1, 2, 2, 1],
                             strides=[1, 2, 2, 1], padding='SAME')
     
-    # 入力をIMAGE_SIZExIMAGE_SIZEx3に変形
     x_image = tf.reshape(images_placeholder, [-1, IMAGE_SIZE, IMAGE_SIZE, NUM_RGB_CHANNEL])
+    # output shape [batch_size  , IMAGE_WIDTH, IMAGE_HEIGHT, NUM_RGB_CHANNEL]
 
-    # 畳み込み層1の作成
+    # 畳み込み層1
     with tf.name_scope('conv1') as scope:
         W_conv1 = weight_variable([FILTER1_SIZE, FILTER1_SIZE, NUM_RGB_CHANNEL, manip])
         b_conv1 = bias_variable([manip])
         h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
+    # output shape [batch_size  , IMAGE_WIDTH, IMAGE_HEIGHT, manip]
 
-    # プーリング層1の作成
+    # プーリング層1
     with tf.name_scope('pool1') as scope:
         h_pool1 = max_pool_2x2(h_conv1)
+    # output shape [batch_size  , IMAGE_WIDTH, IMAGE_HEIGHT, manip//2]
     
-    # 畳み込み層2の作成
+    # 畳み込み層2
     with tf.name_scope('conv2') as scope:
         W_conv2 = weight_variable([FILTER2_SIZE, FILTER2_SIZE, manip, manip2])
         b_conv2 = bias_variable([manip2])
         h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
+    # output shape [batch_size  , IMAGE_WIDTH, IMAGE_HEIGHT, manip2]
 
     # プーリング層2の作成
     with tf.name_scope('pool2') as scope:
         h_pool2 = max_pool_2x2(h_conv2)
+    # output shape [batch_size  , IMAGE_WIDTH, IMAGE_HEIGHT, manip2//2]
 
-    # 全結合層1の作成
+    # 全結合層1
     with tf.name_scope('fc1') as scope:
         W_fc1 = weight_variable([IMAGE_SIZE_2*IMAGE_SIZE_2*manip2, fc1_manip])
         b_fc1 = bias_variable([fc1_manip])
@@ -95,171 +92,101 @@ def inference(images_placeholder, keep_prob):
         h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
         # dropoutの設定
         h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
+    # output shape [batch_size  , fc1_manip]
 
-    # 全結合層2の作成
+    # 全結合層2
     with tf.name_scope('fc2') as scope:
         W_fc2 = weight_variable([fc1_manip, NUM_CLASSES])
         b_fc2 = bias_variable([NUM_CLASSES])
 
-    # ソフトマックス関数による正規化
+    # ソフトマックス関数
     with tf.name_scope('softmax') as scope:
         y_conv=tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)
+    # output shape [batch_size, NUM_CLASSES]
 
-    # 各ラベルの確率のようなものを返す
     return y_conv
 
 def loss(logits, labels):
-    """ lossを計算する関数
-
-    引数:
-      logits: ロジットのtensor, float - [batch_size, NUM_CLASSES]
-      labels: ラベルのtensor, int32 - [batch_size, NUM_CLASSES]
-
-    返り値:
-      cross_entropy: 交差エントロピーのtensor, float
-
-    """
-
-    # 交差エントロピーの計算
-    #cross_entropy = -tf.reduce_sum(labels*tf.log(logits))
+    """ 誤差関数: 最適化の評価用に交差エントロピーの計算をする """
     cross_entropy = -tf.reduce_sum(labels*tf.log(tf.clip_by_value(logits,1e-10,1.0)))
-    # TensorBoardで表示するよう指定
-    tf.summary.scalar("cross_entropy", cross_entropy)
+    tf.summary.scalar("cross_entropy", cross_entropy) # for tensorboard
     return cross_entropy
 
 def training(loss, learning_rate):
-    """ 訓練のOpを定義する関数
-
-    引数:
-      loss: 損失のtensor, loss()の結果
-      learning_rate: 学習係数
-
-    返り値:
-      train_step: 訓練のOp
-
-    """
-
+    """ トレーニング: モデルの変数をlossを目安に最適化する """
     train_step = tf.train.AdamOptimizer(learning_rate).minimize(loss)
     return train_step
 
 def accuracy(logits, labels):
-    """ 正解率(accuracy)を計算する関数
-
-    引数: 
-      logits: inference()の結果
-      labels: ラベルのtensor, int32 - [batch_size, NUM_CLASSES]
-
-    返り値:
-      accuracy: 正解率(float)
-
-    """
+    """ 正解率(accuracy)を計算する関数 """
     correct_prediction = tf.equal(tf.argmax(logits, 1), tf.argmax(labels, 1))
     accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
-    tf.summary.scalar("accuracy", accuracy)
+    tf.summary.scalar("accuracy", accuracy) # for tensorboard
     return accuracy
 
-if __name__ == '__main__':
-    # ファイルを開く
-    f = open(FLAGS.train, 'r')
-    # データを入れる配列
-    train_image = []
-    train_label = []
-    for line in f:
-        # 改行を除いてスペース区切りにする
-        line = line.rstrip()
-        l = line.split()
-        # データを読み込んでIMAGE_SIZExIMAGE_SIZEに縮小
-        img = cv2.imread(l[0])
+def loadImages(labelFilePath):
+    file = open(labelFilePath, 'r')
+    image = []
+    label = []
+    for line in file:
+        imgpath, labelIndex= line.rstrip().split()
+        img = cv2.imread(imgpath)
         img = cv2.resize(img, (IMAGE_SIZE, IMAGE_SIZE))
-        # 一列にした後、0-1のfloat値にする
-        train_image.append(img.flatten().astype(np.float32)/255.0)
-        # ラベルを1-of-k方式で用意する
-        tmp = np.zeros(NUM_CLASSES)
-        tmp[int(l[1])] = 1
-        train_label.append(tmp)
-    # numpy形式に変換
-    train_image = np.asarray(train_image)
-    train_label = np.asarray(train_label)
-    f.close()
+        image.append(img.flatten().astype(np.float32)/255.0)
+        labelData = np.zeros(NUM_CLASSES)
+        labelData[int(labelIndex)] = 1
+        label.append(labelData)
+    file.close()
+    return (np.asarray(image), np.asarray(label))
 
-    f = open(FLAGS.test, 'r')
-    test_image = []
-    test_label = []
-    for line in f:
-        line = line.rstrip()
-        l = line.split()
-        img = cv2.imread(l[0])
-        img = cv2.resize(img, (IMAGE_SIZE, IMAGE_SIZE))
-        test_image.append(img.flatten().astype(np.float32)/255.0)
-        tmp = np.zeros(NUM_CLASSES)
-        tmp[int(l[1])] = 1
-        test_label.append(tmp)
-    test_image = np.asarray(test_image)
-    test_label = np.asarray(test_label)
-    f.close()
+if __name__ == '__main__':
+    train_image, train_label = loadImages(FLAGS.train)
+    test_image, test_label =  loadImages(FLAGS.test)
     
     with tf.Graph().as_default():
-        # 画像を入れる仮のTensor
         images_placeholder = tf.placeholder("float", shape=(None, IMAGE_PIXELS))
-        # ラベルを入れる仮のTensor
         labels_placeholder = tf.placeholder("float", shape=(None, NUM_CLASSES))
-        # dropout率を入れる仮のTensor
         keep_prob = tf.placeholder("float")
 
-        # inference()を呼び出してモデルを作る
         logits = inference(images_placeholder, keep_prob)
-        # loss()を呼び出して損失を計算
         loss_value = loss(logits, labels_placeholder)
+        train_op = training(loss_value, FLAGS.learning_rate)
 
-        learning_rate = FLAGS.learning_rate
-            # training()を呼び出して訓練
-        train_op = training(loss_value, learning_rate)
-        # 精度の計算
         acc = accuracy(logits, labels_placeholder)
 
-        # 保存の準備
         saver = tf.train.Saver()
-        # Sessionの作成
         sess = tf.Session()
-        # 変数の初期化
         sess.run(tf.global_variables_initializer())
-        # TensorBoardで表示する値の設定
+
         summary_op = tf.summary.merge_all()
         summary_writer = tf.summary.FileWriter(FLAGS.train_dir, sess.graph)
+
+        feedDictNoProb = {
+            images_placeholder: train_image,
+            labels_placeholder: train_label,
+            keep_prob: 1.0}
         
         # 訓練の実行
         n = int(len(train_image)/FLAGS.batch_size)
         for step in range(FLAGS.max_steps):
             startTime = time.time()
             for i in range(n):
-                # batch_size分の画像に対して訓練の実行
                 batch = FLAGS.batch_size*i
-                # feed_dictでplaceholderに入れるデータを指定する
                 sess.run(train_op, feed_dict={
                   images_placeholder: train_image[batch:batch+FLAGS.batch_size],
                   labels_placeholder: train_label[batch:batch+FLAGS.batch_size],
                   keep_prob: 0.5})
 
-            # 1 step終わるたびに精度を計算する
-            train_accuracy = sess.run(acc, feed_dict={
-                images_placeholder: train_image,
-                labels_placeholder: train_label,
-                keep_prob: 1.0})
-            print("step %d, training accuracy %g, batch/sec %d"%(step, train_accuracy , int(n/(time.time() - startTime))))
+            train_accuracy = sess.run(acc, feed_dict=feedDictNoProb)
+            print("step %d, training accuracy %g, %d batch/sec"%(step, train_accuracy , int(n/(time.time() - startTime))))
 
-            # 1 step終わるたびにTensorBoardに表示する値を追加する
-            summary_str = sess.run(summary_op, feed_dict={
-                images_placeholder: train_image,
-                labels_placeholder: train_label,
-                keep_prob: 1.0})
+            summary_str = sess.run(summary_op, feed_dict=feedDictNoProb)
             summary_writer.add_summary(summary_str, step)
 
-    # 訓練が終了したらテストデータに対する精度を表示
-    print("test accuracy %g"%sess.run(acc, feed_dict={
-        images_placeholder: test_image,
-        labels_placeholder: test_label,
-        keep_prob: 1.0}))
+        print("test accuracy %g"%sess.run(acc, feed_dict={
+            images_placeholder: test_image,
+            labels_placeholder: test_label,
+            keep_prob: 1.0}))
 
-    # 最終的なモデルを保存
     cwd = os.getcwd()
     save_path = saver.save(sess, cwd+"\\model.ckpt")
