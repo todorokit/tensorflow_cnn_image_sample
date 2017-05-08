96bb29b6a1cf52ee0222e40660585953e52f27c2
todorokit <fstest1234ifua@gmail.com>
2017-05-09 02:27:37 +0900

    学習率探索ツール、メモリリーク修正。リファクタリング。(次はPICTデータを取り込みたい。)

--- search-learning-rate.py
+++ search-learning-rate.py
@@ -21,33 +21,34 @@ FLAGS = flags.FLAGS
 
 flags.DEFINE_string('train', 'train.txt', 'File name of train data')
 flags.DEFINE_string('train_dir', 'c:\\tmp\\image_cnn', 'Directory to put the training data.')
-flags.DEFINE_integer('max_steps', 10, 'Number of steps to run trainer.')
+flags.DEFINE_integer('max_steps', 3, 'Number of steps to run trainer.')
 flags.DEFINE_string('batch_sizes', "5,10", 'Batch size'
                     'Must divide evenly into the dataset sizes.')
 flags.DEFINE_float('learning_rate_base', 2e-4, 'Initial learning rate.')
-flags.DEFINE_float('learning_rate_odds', 0.9, 'Initial learning rate.')
-flags.DEFINE_float('num_try', 3, 'Initial learning rate.')
+flags.DEFINE_float('learning_rate_odds', 0.95, 'Initial learning rate.')
+flags.DEFINE_float('num_loop', 20, 'Initial learning rate.')
 
 if __name__ == '__main__':
     train_image, train_label, _ = deeptool.loadImages(FLAGS.train, IMAGE_SIZE, NUM_CLASSES)
 #    test_image, test_label =  deeptool.loadImages(FLAGS.test, IMAGE_SIZE, NUM_CLASSES)
     batch_sizes = [ int(sizestr) for sizestr in FLAGS.batch_sizes.split(",")]
     result = OrderedDict()
-    for step in range(FLAGS.max_steps):
-        for batch_size in batch_sizes:
-            timeStart = time.time() 
+
+    for loop, batch_size in [(loop, batch_size) for loop in range(FLAGS.num_loop) for batch_size in batch_sizes]:
+        
+        with tf.Graph().as_default(), tf.Session() as sess:
+            timeStart = time.time()
             images_placeholder = tf.placeholder("float", shape=(None, IMAGE_SIZE*IMAGE_SIZE*NUM_RGB_CHANNEL))
             labels_placeholder = tf.placeholder("float", shape=(None, NUM_CLASSES))
             keep_prob = tf.placeholder("float")
             
             logits = modelcnn.inference(images_placeholder, keep_prob)
             loss_value = modelcnn.loss(logits, labels_placeholder)
-            learningRate = FLAGS.learning_rate_base * (FLAGS.learning_rate_odds** step)
-            train_op = modelcnn.training(loss_value, learningRate)
-
             acc = modelcnn.accuracy(logits, labels_placeholder)
 
-            sess = tf.Session()
+            learningRate = FLAGS.learning_rate_base * (FLAGS.learning_rate_odds** loop)
+            train_op = modelcnn.training(loss_value, learningRate)
+
             sess.run(tf.global_variables_initializer())
             
             feedDictNoProb = {
@@ -57,21 +58,20 @@ if __name__ == '__main__':
             
             # 訓練の実行
             n = int(len(train_image)/batch_size)
-            for instep in range(FLAGS.num_try):
+            for step in range(FLAGS.max_steps):
                 for i in range(n):
                     batch = batch_size*i
                     sess.run(train_op, feed_dict={
                         images_placeholder: train_image[batch:batch+batch_size],
                         labels_placeholder: train_label[batch:batch+batch_size],
-                        keep_prob: 0.5})
-                    
+                        keep_prob: 0.50})
+                
             timeTrain = time.time()
             accVal = sess.run(acc, feed_dict={
                 images_placeholder: train_image,
                 labels_placeholder: train_label,
                 keep_prob: 1.0})
 
-            sess.close()
             # varbose = "(Time train %.2f, test %.2f)" % (timeTrain - timeStart, time.time() - timeTrain)
             fom = "test learningRate %.3e, batch_size %d, accuracy %g"
             print(fom%(learningRate, batch_size, accVal))
@@ -84,6 +84,7 @@ if __name__ == '__main__':
                 dic = OrderedDict()
                 dic[j] = accVal
                 result[i] = dic
+            #sess.close()
     print("---------------result-----------------")
     ## TODO: writeTsv() 
     sys.stdout.write("lr\t")
