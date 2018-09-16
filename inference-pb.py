fd2d7eb001f5f8cc59f73b0b87f655bc0c832476
todorokit <fstest1234ifua@gmail.com>
2018-09-16 16:47:15 +0900

    make-pbを改良

--- inference-pb.py
+++ inference-pb.py
@@ -25,16 +25,6 @@ def main(argv):
         with open('graph.pb', 'rb') as fp:
             graph_def = tf.GraphDef()
             graph_def.ParseFromString(fp.read())
-
-            for node in graph_def.node:
-                if node.op == 'RefSwitch':
-                    node.op = 'Switch'
-                    for index in range(len(node.input)):
-                        if 'moving_' in node.input[index]:
-                            node.input[index] = node.input[index] + '/read'
-                elif node.op == 'AssignSub':
-                    node.op = 'Sub'
-                    if 'use_locking' in node.attr: del node.attr['use_locking']
             tf.import_graph_def(graph_def, name="")
         arr = sess.run('fp32_storage/tower_0/fc/fc_var/fc_act:0', feed_dict={'input_image:0': images, 'keep_prob:0': 1.0, 'phase_train:0': False})[0]
         indices = top5(arr)
